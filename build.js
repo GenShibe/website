183de98295d8367e06b217e65b9913cc6c42fe80
Gen <buzhybrid@gmail.com>
2024-03-19 16:11:58 -0700

    use locale for last updated

--- build.js
+++ build.js
@@ -3,6 +3,6 @@ const fs = require("fs");
 let file = fs.readFileSync("index.html", "utf-8");
 file = file.replace(
   '<span class="last-updated"></span>',
-  new Date().toUTCString(),
+  new Date().toString(),
 );
 fs.writeFileSync("index.html", file);
