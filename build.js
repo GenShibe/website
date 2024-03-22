b75c7793a40517ea2f166d99793ba16c23340c7c
Gen <buzhybrid@gmail.com>
2024-03-22 00:04:26 -0700

    fix the updated time

--- build.js
+++ build.js
@@ -3,6 +3,6 @@ const fs = require("fs");
 let file = fs.readFileSync("index.html", "utf-8");
 file = file.replace(
   '<span class="last-updated"></span>',
-  new Date().toString(),
+  new Date().toUTCString(),
 );
 fs.writeFileSync("index.html", file);
